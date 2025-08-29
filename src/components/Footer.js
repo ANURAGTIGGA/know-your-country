71fcf9189fe1a378df45530974d60da45b4c7d0d
Anurag Aron Tigga <anuragarontigga@Anurags-MacBook-Pro.local>
2025-08-29 12:05:08 +0530

    spacing fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ import tailwindLogo from '/tailwind.svg'
 
 export function createFooter() {
   return `
-    <footer>
-      <p>&copy; 2025 Know Your Country. All rights reserved.</p>
+    <footer class="flex items-center justify-center p-4 border-t">
+      <p class="mr-5">&copy; 2025 Know Your Country. </p>
       <div class="logos flex justify-center space-x-4">
-        <span class="flex items-center">Powered by:</span>
+        <span class="flex items-center mr-0">Powered by:</span>
         <img src="${viteLogo}" class="logo" alt="Vite logo" />
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
         <img src="${tailwindLogo}" class="logo tailwind" alt="Tailwind logo" />
